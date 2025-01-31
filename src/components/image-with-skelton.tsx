7cccf6b0270ac71dea6617359dce2f8210f8ecaf
Nagamatsu Ryosuke <koukapoke@gmail.com>
2025-01-31 10:16:22 +0900

    依存関係の更新と画像コンポーネントに拡大表示機能を追加

--- src/components/image-with-skelton.tsx
+++ src/components/image-with-skelton.tsx
@@ -1,13 +1,27 @@
 import Image from "next/image";
 import { useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { ZoomIn } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 interface ImageWithSkeletonProps {
   src: string;
   alt: string;
+  onClick?: () => void;
 }
 
-export const ImageWithSkeleton = ({ src, alt }: ImageWithSkeletonProps) => {
+export const ImageWithSkeleton = ({
+  src,
+  alt,
+  onClick,
+}: ImageWithSkeletonProps) => {
   const [loaded, setLoaded] = useState(false);
   return (
     <div className="relative w-full h-full">
@@ -18,7 +32,33 @@ export const ImageWithSkeleton = ({ src, alt }: ImageWithSkeletonProps) => {
         height={200}
         onLoad={() => setLoaded(true)}
         className="w-full h-auto"
+        onClick={onClick}
       />
+      <Dialog>
+        <DialogTrigger asChild>
+          <button
+            type="button"
+            className="absolute top-6 right-1 flex items-center justify-center w-6 h-6 p-0 text-black rounded-full bg-white/40"
+            title="拡大表示"
+          >
+            <ZoomIn className="w-5 h-5" />
+          </button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>拡大表示</DialogTitle>
+            <DialogDescription>{alt}</DialogDescription>
+          </DialogHeader>
+          <Image
+            src={src}
+            alt={alt}
+            width={500}
+            height={700}
+            onLoad={() => setLoaded(true)}
+            className="w-full h-auto"
+          />
+        </DialogContent>
+      </Dialog>
       {!loaded && (
         <Skeleton className="absolute inset-0 w-full h-auto aspect-[143/200]" />
       )}
