6608f8510471c22bc86ceb6d8f05ae4df8422b66
NagatsukiSep <nagamatsu.r.ab@m.titech.ac.jp>
2025-07-22 22:48:29 +0900

    画像ドメインリストに新しいドメインを追加

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['api.ultraman-cardgame.com', "ucg-deck-editor.innovatz.jp"],
+    domains: ['api.ultraman-cardgame.com', "ucg-deck-editor.innovatz.jp", "ud-builder.innovatz.jp"],
     unoptimized: true,
   },
 };
