5c938cf280c34866d4998aa69945dc1a9038f34d
NagatsukiSep <nagamatsu.r.ab@m.titech.ac.jp>
2025-07-23 00:08:58 +0900

    ラジオ切り替え時のクエリ初期化の修正

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -289,6 +289,7 @@ export default function Home() {
                     level: "none",
                     type: "none",
                     round: "none",
+                    keyword: "",
                   });
                 }}
               >
