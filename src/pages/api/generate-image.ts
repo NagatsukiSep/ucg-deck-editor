850ec9eb1fdabe0f303753434fa7b06380a9483d
NagatsukiSep <nagamatsu.r.ab@m.titech.ac.jp>
2025-08-05 15:19:45 +0900

    フォントファミリーを「Noto Sans, sans-serif」に変更

--- src/pages/api/generate-image.ts
+++ src/pages/api/generate-image.ts
@@ -92,7 +92,7 @@ export default async function handler(
       <text x="50%" y="${COUNT_WIDTH / 2 + verticalAdjust}" 
             font-size="${fontSize}" font-weight="bold"
             text-anchor="middle"
-            fill="white" font-family="DejaVu Sans, sans-serif">
+            fill="white" font-family="Noto Sans, sans-serif">
         ${text}
       </text>
     </svg>
